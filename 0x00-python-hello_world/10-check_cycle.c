328bb72ac8f45ff632c96487e53653903ed0eb0d
segunlawal <lawalsegun72@gmail.com>
2023-03-06 16:48:15 -0800

    add files needed for task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: list to be checked
+ * Return: 0 if there is no cycle or 1 otherwise
+ */
+
+int check_cycle(listint_t *list)
+{
+
+}
