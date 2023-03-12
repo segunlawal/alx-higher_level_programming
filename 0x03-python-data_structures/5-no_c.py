82ed8de0bac7bea8f0396f8de809f91c2d170dd0
segunlawal <lawalsegun72@gmail.com>
2023-03-12 15:53:48 -0700

    fix iteration issue

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
     str_list = list(my_string)
+    new_list = []
     for char in str_list:
-        if char == "c" or char == "C":
-            str_list.remove(char)
-    str = "".join(str_list)
+        if char != "c" and char != "C":
+            new_list.append(char)
+    str = "".join(new_list)
     return str
