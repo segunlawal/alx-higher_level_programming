3c93e85976d8435c992a730e21f5eaae591f40b4
segunlawal <lawalsegun72@gmail.com>
2023-03-27 10:36:37 -0700

    fix: edit error handling block

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-def safe_print_division(a, b): 
+def safe_print_division(a, b):
     try:
         result = a/b
     except ZeroDivisionError:
-        result = None
         return None
     else:
         return result
