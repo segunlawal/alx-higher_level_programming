f74dc9f9274b58270a1f4da26fcaf2a9ee1c4966
segunlawal <lawalsegun72@gmail.com>
2023-03-29 14:18:02 -0700

    add position attribute to class

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,59 @@
+
+#!/usr/bin/python3
+""""This module defines a square"""
+
+
+class Square:
+    """This class defines a square"""
+    def __init__(self, size=0, position=(0,0)):
+        """Initialization method
+        Args:
+            size: size of the square
+            position: position of square
+        """
+        self.__size = size
+        self.__position = position
+
+    @property
+    def position(self):
+        return (self.__position)
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    @position.setter
+    def position(self, value):
+        if isinstance(value, tuple):
+            self.__position = value
+        raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        """Get area of the square
+        Returns:
+            current square area
+        """
+        return (self.__size**2)
+
+    def my_print(self):
+        """Print square"""
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.position[1]):
+                print()
+            for i in range(self.size):
+                for _ in range(self.position[0]):
+                    print(" ", end="")
+                for _ in range(self.size):
+                    print("#", end="")
+                print()
