97736430bf861160422c66d0ea0ea5b34b5a8568
segunlawal <lawalsegun72@gmail.com>
2023-04-17 17:19:11 -0700

    check for class instance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+
+#!/usr/bin/python3
+"""contains inherits_from function"""
+
+
+def inherits_from(obj, a_class):
+    """Checks if the object is an instance of a class that
+    inherited (directly or indirectly) from the specified class
+    Args:
+        obj: object
+        a_class: class
+    Return:
+        True or False
+    """
+    return (isinstance(obj, a_class))
