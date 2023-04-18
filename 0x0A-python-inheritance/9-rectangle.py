cde7a01273d2d2a96b6807e7cd06e2432185b74e
segunlawal <lawalsegun72@gmail.com>
2023-04-17 18:13:46 -0700

    add instantiation methods to class

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Contains BaseGeometry class"""
+
+
+class BaseGeometry:
+    """class with public instance method"""
+    def area(self):
+        """raises an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates a value
+        Args:
+            name: string
+            value: value
+        """
+        if not isinstance(value, int):
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """defines a rectangle"""
+    def __init__(self, width, height):
+        """rectangle instantiation"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """string representation of the rectangle"""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
