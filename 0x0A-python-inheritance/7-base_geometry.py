2bc32b0ff8f68fe81e2087d91900a3cf1a93e711
segunlawal <lawalsegun72@gmail.com>
2023-04-17 17:55:17 -0700

    add test cases for BaseGeometry class

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -18,4 +18,3 @@ class BaseGeometry:
             raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{:s} must be greater than 0".format(name))
-        
