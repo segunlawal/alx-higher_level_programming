e5795e524c445e8ef62a16f020d604883169e244
segunlawal <lawalsegun72@gmail.com>
2023-04-19 16:57:45 -0700

    improve display method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -75,7 +75,11 @@ class Rectangle(Base):
         """prints in stdout the Rectangle
         instance with the character #
         """
-        for i in range(self.__height):
-            for j in range(self.__width):
+        for _ in range(self.y):
+            print()
+        for _ in range(self.height):
+            for _ in range(self.x):
+                print(" ", end="")
+            for _ in range(self.width):
                 print("#", end="")
             print()
