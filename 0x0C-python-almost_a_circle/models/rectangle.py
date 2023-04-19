c66f8f3329c1bb8e0d60a986c0486b49ad4efe4e
segunlawal <lawalsegun72@gmail.com>
2023-04-19 16:16:25 -0700

    switch range arguments

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -71,7 +71,7 @@ class Rectangle(Base):
         """prints in stdout the Rectangle
         instance with the character #
         """
-        for i in range(self.__width):
-            for j in range(self.__height):
+        for i in range(self.__height):
+            for j in range(self.__width):
                 print("#", end="")
             print()
