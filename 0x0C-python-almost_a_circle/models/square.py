5848b0c7c71b6a16c161540e827576a5f034e04d
segunlawal <lawalsegun72@gmail.com>
2023-04-19 17:41:07 -0700

    add Square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Contains a Square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class that inherits from
+    Rectangle class
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """Constructor method
+        Args:
+            x: x position
+            y: y position
+            size: width and height of square
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return ("[Square] ({}) {}/{} - {}"
+                .format(self.id, self.x, self.y, self.size))
