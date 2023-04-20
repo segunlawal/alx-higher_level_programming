09bb7fcde7b41fe7343272147a9e5578a39114d3
segunlawal <lawalsegun72@gmail.com>
2023-04-20 10:36:28 -0700

    add dictionary method to Square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -57,3 +57,7 @@ class Square(Rectangle):
                 self.x = kwargs['x']
             if 'y' in keys:
                 self.y = kwargs['y']
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Square"""
+        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
