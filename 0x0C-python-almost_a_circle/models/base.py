69e75778d3b5cd59ae64024beef5fe6b31b219f6
segunlawal <lawalsegun72@gmail.com>
2023-04-20 11:07:24 -0700

    add class method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,6 +22,15 @@ class Base:
             return "[]"
         return (json.dumps(list_dictionaries))
 
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Returns the list of the JSON string representation
+        """
+        if not json_string:
+            return []
+        return json.loads(json_string)
+
     @classmethod
     def save_to_file(cls, list_objs):
         """Writes the JSON string representation of list_objs to a file"""
