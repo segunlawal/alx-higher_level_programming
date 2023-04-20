3b41709843524c6697e7c813db1754c51a841155
segunlawal <lawalsegun72@gmail.com>
2023-04-20 10:43:35 -0700

    method returns JSON string representation of a dictionary

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Contains a Base class"""
+import json
 
 
 class Base:
@@ -13,3 +14,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None:
+            return ([])
+        return (json.dumps(list_dictionaries))
