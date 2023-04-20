03cad95be607b2185c341ef2b45ccf17ba73c387
segunlawal <lawalsegun72@gmail.com>
2023-04-20 10:50:48 -0700

    update static method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,6 +18,6 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation of list_dictionaries"""
-        if list_dictionaries is None or list_dictionaries == []:
-            return ([])
+        if not list_dictionaries:
+            return "[]"
         return (json.dumps(list_dictionaries))
