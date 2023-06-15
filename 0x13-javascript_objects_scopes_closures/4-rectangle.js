920e5d7be4f8ed4245e7acacc4e3ff3246e55040
Segun <lawalsegun72@gmail.com>
2023-06-15 00:28:13 +0000

    semistandard fix

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -14,16 +14,15 @@ class Rectangle {
     }
   }
 
-  rotate() {
-	let temp;
-	temp = this.width;
-	this.width = this.height;
-	this.height = temp;
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
   }
 
-  double() {
-	this.width *= 2;
-	this.height *= 2;
+  double () {
+    this.width *= 2;
+    this.height *= 2;
   }
 }
 
