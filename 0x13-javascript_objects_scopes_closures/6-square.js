b35f059f90f65cb0c0f0f18d44a10759165dbfa0
Segun <lawalsegun72@gmail.com>
2023-06-15 00:52:25 +0000

    fix charPrint function

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,8 +4,12 @@ const Rectangle = require('./4-rectangle.js');
 class Square extends Rectangle {
   charPrint (c) {
     if (c) {
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
+     for (let i = 0; i < this.height; i++) {
+        let row = '';
+        for (let j = 0; j < this.width; j++) {
+          row += c;
+        }
+        console.log(row);
       }
     } else { super.print(); }
   }
