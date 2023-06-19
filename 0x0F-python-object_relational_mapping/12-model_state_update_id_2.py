6ba16f0f4462a0513720485b7059c1b467a2ca9f
Segun <lawalsegun72@gmail.com>
2023-06-19 08:38:57 -0700

    task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""script that changes the name of a State object from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    Access database and list the states from the database
+    """
+
+    url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+
+    engine = create_engine(url)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    state = session.query(State).filter(State.id === 2).first()
+    state.name = "New Mexico"
