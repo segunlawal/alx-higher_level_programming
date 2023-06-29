462a60b6524ccb6118699f7e1db1ee478e1d6def
Segun <lawalsegun72@gmail.com>
2023-06-29 08:30:42 -0700

    reduce to three lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL
-# and displays the size of the body of the response
-
+# Sends a request to that URL and displays the size of the body of the response
 curl -s -o "$response_file" -w "%{size_download}" "$1"
