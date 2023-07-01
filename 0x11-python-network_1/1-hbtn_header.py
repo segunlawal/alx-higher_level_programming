62e0b3d70b95f2105bc216f0c430b956e636b092
Segun <lawalsegun72@gmail.com>
2023-07-01 05:52:01 -0700

    move sys call

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,8 +8,8 @@ import urllib.request
 import sys
 
 
-url = sys.argv[1]
-
 if __name__ == '__main__':
+    url = sys.argv[1]
+
     with urllib.request.urlopen(url) as response:
         print(dict(response.headers).get("X-Request-Id"))
