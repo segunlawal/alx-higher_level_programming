2e77fe4611e2ffa7df72e893657e142704fe71d6
Segun <lawalsegun72@gmail.com>
2023-07-01 08:13:37 -0700

    pycodestyle fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,7 +9,7 @@ from requests.auth import HTTPBasicAuth
 
 
 if __name__ == '__main__':
-    url= "https://api.github.com/user"
+    url = "https://api.github.com/user"
     username = sys.argv[1]
     password = sys.argv[2]
     auth = HTTPBasicAuth(username, password)
