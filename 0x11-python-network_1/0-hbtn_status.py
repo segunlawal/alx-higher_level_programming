2a758e8211fec7a8a9056ac1756b29ec69f3a8f7
Segun <lawalsegun72@gmail.com>
2023-07-01 02:08:32 -0700

    format type

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     with urllib.request.urlopen(url) as response:
         html = response.read()
 
-        print('Body response:')
-        print('\t- type:', type(html))
-        print('\t- content: ', html)
-        print('\t- utf8 content:', html.decode('utf-8'))
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
