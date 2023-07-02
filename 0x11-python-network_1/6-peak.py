4cd9e35eb1493c904bcdd898d380e6e2b4be5ebd
Segun <lawalsegun72@gmail.com>
2023-07-02 15:02:49 -0700

    find peak value

---
+++ 0x11-python-network_1/6-peak.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Function that finds a peak in a list of unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    """finds a peak in a list of unsorted integers"""
+    n = len(list_of_integers)
+
+    if list_of_integers == []:
+        return None
+    elif n == 1:
+        return list_of_integers[0]
+    elif n == 2:
+        return max(list_of_integers[0], list_of_integers[1])
+
+    mid = int(n / 2)
+    peak = list_of_integers[mid]
+
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[(mid + 1):])
