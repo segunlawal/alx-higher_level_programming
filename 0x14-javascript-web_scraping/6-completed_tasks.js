9ccdb45345ff696ceb09ee46453227d4f75f3412
Segun <lawalsegun72@gmail.com>
2023-07-25 21:11:21 +0000

    get number of tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+    if (!error) {
+	const res = JSON.parse(body);
+	const completed = {};
+    for (const task of res) {
+      if (task.completed === true) {
+        if (completed[task.userId]) {
+          completed[task.userId] += 1;
+        } else {
+          completed[task.userId] = 1;
+        }
+      }
+    }
+    console.log(completed);
+    }
+	if (error) console.log(error);
+  }
+);
