2404972ef823916376786d8d3c73fd54b864a179
Segun <lawalsegun72@gmail.com>
2023-07-25 14:37:32 -0700

    update script

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,21 +3,20 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (error, response, body) => {
-  if (!error) {
-    const res = JSON.parse(body);
-    const tasksCompleted = {};
-    res.forEach((todo) => {
-      const key = todo.userId;
-      if (todo.completed) {
-        if (!tasksCompleted[key]) {
-          tasksCompleted[key] = 1;
-        } else {
-          tasksCompleted[key] += 1;
-        }
-      }
-    });
-    console.log(tasksCompleted);
-  }
   if (error) console.log(error);
+
+  const res = JSON.parse(body);
+  const tasksCompleted = {};
+  res.forEach((todo) => {
+    const key = todo.userId;
+    if (todo.completed) {
+      if (!tasksCompleted[key]) {
+        tasksCompleted[key] = 1;
+      } else {
+        tasksCompleted[key] += 1;
+      }
+    }
+  });
+  console.log(tasksCompleted);
 }
 );
